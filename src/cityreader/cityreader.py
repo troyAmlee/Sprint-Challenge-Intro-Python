6c93e01431efb1478ca3f54e41ab93881452a6e4
troyAmlee <troyamlee19@gmail.com>
2020-09-04 11:11:40 -0500

    [MVP] Completed cityreader, transitioning to stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,15 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+# city,state_name,county_name,lat,lng,population,density,timezone,zips
+
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+  def __str__(self):
+    return f"{self.name} {self.lat} {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -17,11 +26,17 @@
 cities = []
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
+  # Implement the functionality to read from the 'cities.csv' file
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  with open('cities.csv') as c:
+    for i in c:
+      splitted = i.split(',')
+      # print(splitted[0])
+      if (splitted[0] != 'city'):
+        cities.append(City(splitted[0], float(splitted[3]), float(splitted[4])))
+  
     return cities
 
 cityreader(cities)
